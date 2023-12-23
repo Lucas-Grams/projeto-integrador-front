bce22bde26c4b5695a55be043a6d98afb30beba7
matheusfriedhein <matheusfriedhein@gmail.com>
2023-12-23 08:36:14 -0300

    PNIP-6 - login e logout

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,3 +1,7 @@
+
+//@ts-ignore
+import Dropdown from "src/assets/js/dropdown/dropdown.js";
+
 import {Component, OnInit} from '@angular/core';
 import {NavigationEnd, Router} from "@angular/router";
 import {LoginService} from "../../core/services/login.service";
@@ -5,7 +9,7 @@ import {LoginService} from "../../core/services/login.service";
 @Component({
    selector: 'pnip-header',
    templateUrl: './header.component.html',
-   styleUrls: []
+   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
 
@@ -43,11 +47,24 @@ export class HeaderComponent implements OnInit {
             const url = event.url;
             if (!url) {
                return;
-            }
-            else if (url.includes('/portal-admin')) this.portal = [{name: 'Portal ADMIN', href: url}];
-            else if (url.includes('/portal-mpa'))   this.portal = [{name: 'Portal MPA', href: url}];
-            else if (url.includes('/portal-tr'))    this.portal = [{name: 'Portal TR', href: url}];
+            } else if (url.includes('/portal-admin')) this.portal = [{name: 'Portal ADMIN', href: url}];
+            else if (url.includes('/portal-mpa')) this.portal = [{name: 'Portal MPA', href: url}];
+            else if (url.includes('/portal-tr')) this.portal = [{name: 'Portal TR', href: url}];
          }
+      });
+   }
+
+   openDrop() {
+      document.querySelectorAll('[data-toggle="dropdown"]').forEach((trigger) => {
+         console.log(trigger)
+         const config = {
+            iconToHide: 'fa-chevron-up',
+            iconToShow: 'fa-chevron-down',
+            trigger,
+            useIcons: true,
+         }
+         const dropdown = new Dropdown(config);
+            dropdown.setBehavior();
       });
    }
 
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,18 @@
 import {Component, OnInit} from '@angular/core';
 import {LoginService} from "../../core/services/login.service";
 
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
 @Component({
    selector: 'pnip-login',
    templateUrl: './login.component.html',
    styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit{
+export class LoginComponent implements OnInit {
+
+   public formGroup!: FormGroup;
+   public identificacao?: string;
+   public senha?: string;
 
    public breadcrumb = [
       {
@@ -21,24 +27,37 @@ export class LoginComponent implements OnInit{
    ];
    private id: any;
 
-   constructor(private loginService: LoginService) {
+   constructor(private fb: FormBuilder, private loginService: LoginService) {
 
    }
 
-   login() {
-      this.loginService.login();
-   }
-   
    ngOnInit() {
+      this.formGroup = this.fb.group({
+         identificacao: [this.identificacao, [Validators.required]],
+         senha: [this.senha, [Validators.required]],
+      });
+
       this.loginService.isLogin();
       this.id = setInterval(() => {
          this.loginService.isLogin();
       }, 300);
    }
 
+   login() {
+      this.loginService.login();
+   }
+
    ngOnDestroy() {
       if (this.id) {
          clearInterval(this.id);
+      }
+   }
+
+   submit() {
+      if (this.formGroup.valid) {
+         console.log(this.formGroup.value);
+      } else {
+         this.formGroup.markAllAsTouched();
       }
    }
 }
